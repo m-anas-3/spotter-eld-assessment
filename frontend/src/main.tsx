eb7a31783821db6b45543e34251e909bc9b464c2
m-anas-3 <muhammadanas222888@gmail.com>
2026-07-23 02:41:24 +0500

    feat: add Vercel Analytics integration for enhanced tracking

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { CssBaseline, ThemeProvider } from '@mui/material'
+import { Analytics } from '@vercel/analytics/react'
 import 'maplibre-gl/dist/maplibre-gl.css'
 import App from './App'
 import { appTheme } from './theme'
@@ -24,5 +25,6 @@ createRoot(document.getElementById('root')!).render(
         <App />
       </ThemeProvider>
     </QueryClientProvider>
+    <Analytics />
   </StrictMode>,
 )
